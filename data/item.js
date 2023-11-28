49f4c8575413e2d36bc78720c241b7fff49e62b9
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-28 22:54:48 +0600

    data added

---
+++ data/item.js
@@ -0,0 +1,93 @@
+const items = [
+    {
+        id: '001',
+        itemImage: './images/item-13.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    {
+        id: '002',
+        itemImage: './images/item-25.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    {
+        id: '003',
+        itemImage: './images/item-27.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    {
+        id: '004',
+        itemImage: './images/item-26.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    {
+        id: '005',
+        itemImage: './images/item-19.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    {
+        id: '006',
+        itemImage: './images/item-21.jpg',
+        rating: {
+            star:4.5,
+            numOfRevies: 1400
+        },
+        companyName: 'Carlton London',
+        itemName: 'Men Bio-Wash Joggers',
+        currentPrice: 503,
+        originalPrice: 710,
+        discount: 42,
+        returnPeriod: 14,
+        deliveryDate: '20 Dec 2023'
+    },
+    
+]
